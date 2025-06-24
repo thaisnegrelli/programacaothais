cc24c7b6a472870aae4d3658720fa6e229265949
Thais Negrelli <114539232+thaisnegrelli@users.noreply.github.com>
2025-06-24 11:16:13 -0300

    chamar funcao

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function inserirNome(){
    let nomeUsuario = prompt("Qual o seu nome");
    let elemento = document.querySelector("#nome-usuario");
    elemento.textContent = nomeUsuario
+
+}
+inserirNome();
